a07a49351b88c02eba7dc99505fcb1e254bef27a
Daniel Lima de Souza <103461769+DanielsPng@users.noreply.github.com>
2022-09-08 16:31:21 -0300

    design

--- portfolio/design/assets/Js/gsap.js
+++ portfolio/design/assets/Js/gsap.js
@@ -1,54 +1,41 @@
-var swiper = new Swiper('.product-slider', {
-  spaceBetween: 30,
-  effect: 'fade',
-  // initialSlide: 2,
-  loop: false,
-  navigation: {
-      nextEl: '.next',
-      prevEl: '.prev'
-  },
-  // mousewheel: {
-  //     // invert: false
-  // },
-  on: {
-      init: function(){
-          var index = this.activeIndex;
+var $body = $('body'),
+	$panel = $('.panel'),
+	$pContent = $('.panel__content'),
+	$img = $('.panel__img-col');
 
-          var target = $('.product-slider__item').eq(index).data('target');
+function initTilt() {
+	TweenMax.set([$pContent, $img], { transformStyle: "preserve-3d" });
 
-          console.log(target);
+	$body.mousemove(function(e) {
+		tilt(e.pageX, e.pageY) 
+	});
+};
 
-          $('.product-img__item').removeClass('active');
-          $('.product-img__item#'+ target).addClass('active');
-      }
-  }
+function tilt(cx, cy) {
+	// var sxPos = cx / $panel.width() * 100 - 100;
+	// var syPos = cy / $panel.height() * 100 - 100;
+	var sxPos = (cx / $body.width()*100 - 50)*2 ;
+	var syPos = (cy / $body.height()*100 - 50)*2;
+	TweenMax.to($pContent, 2, {
+		rotationY: -0.03 * sxPos,
+		rotationX: 0.03 * syPos,
+		transformPerspective: 500,
+		transformOrigin: "center center -400",
+		ease: Expo.easeOut
+	});
+	TweenMax.to($img, 2, {
+		rotationY: -0.03 * sxPos,
+		rotationX: 0.03 * syPos,
+		transformPerspective: 500,
+		transformOrigin: "center center -200",
+		ease: Expo.easeOut
+	});
+}
 
-});
+$body.mouseleave(function() {
+	tilt($body.width()/2, $body.height()/2);
+})
 
-swiper.on('slideChange', function () {
-  var index = this.activeIndex;
+initTilt();
 
-  var target = $('.product-slider__item').eq(index).data('target');
-
-  console.log(target);
-
-  $('.product-img__item').removeClass('active');
-  $('.product-img__item#'+ target).addClass('active');
-
-  if(swiper.isEnd) {
-      $('.prev').removeClass('disabled');
-      $('.next').addClass('disabled');
-  } else {
-      $('.next').removeClass('disabled');
-  }
-
-  if(swiper.isBeginning) {
-      $('.prev').addClass('disabled');
-  } else {
-      $('.prev').removeClass('disabled');
-  }
-});
-
-$(".js-fav").on("click", function() {
-  $(this).find('.heart').toggleClass("is-active");
-});
+console.clear();
