df7d4a2e41a2dfb1b5b59cb18c72b0f0cf6af0ef
Daniel Lima de Souza <103461769+DanielsPng@users.noreply.github.com>
2022-09-23 15:34:12 -0300

    potfolio

--- components/header.js
+++ components/header.js
@@ -1,6 +1,6 @@
 class Header extends HTMLElement {
   constructor() {
-    super();
+    super()
   }
 
   connectedCallback() {
@@ -16,60 +16,60 @@ class Header extends HTMLElement {
                 <ul class="nav-list">
                     <li><a href="#">Início</a></li>
                     <li><a href="#">Sobre</a></li>
-                    <li><a href="#">Projetos</a></li>
+                    <li><a href="./portfolio/">Portfólio</a></li>
                     <li><a href="#">Contato</a></li>
                 </ul>
             </nav>
         </header>
-    `;
+    `
   }
 }
 
-customElements.define("header-component", Header);
+customElements.define('header-component', Header)
 
 class MobileNavbar {
   constructor(mobileMenu, navList, navLinks) {
-    this.mobileMenu = document.querySelector(mobileMenu);
-    this.navList = document.querySelector(navList);
-    this.navLinks = document.querySelectorAll(navLinks);
-    this.activeClass = "active";
-    this.body = document.body;
+    this.mobileMenu = document.querySelector(mobileMenu)
+    this.navList = document.querySelector(navList)
+    this.navLinks = document.querySelectorAll(navLinks)
+    this.activeClass = 'active'
+    this.body = document.body
 
-    this.handleClick = this.handleClick.bind(this);
+    this.handleClick = this.handleClick.bind(this)
   }
 
   animateLinks() {
     this.navLinks.forEach((link, index) => {
       link.style.animation
-        ? (link.style.animation = "")
+        ? (link.style.animation = '')
         : (link.style.animation = `navLinkFade 0.5s ease forwards ${
             index / 7 + 0.3
-          }s`);
-    });
+          }s`)
+    })
   }
 
   handleClick() {
-    this.navList.classList.toggle(this.activeClass);
-    this.mobileMenu.classList.toggle(this.activeClass);
-    this.animateLinks();
-    this.body.classList.toggle(this.activeClass);
+    this.navList.classList.toggle(this.activeClass)
+    this.mobileMenu.classList.toggle(this.activeClass)
+    this.animateLinks()
+    this.body.classList.toggle(this.activeClass)
   }
 
   addClickEvent() {
-    this.mobileMenu.addEventListener("click", this.handleClick);
+    this.mobileMenu.addEventListener('click', this.handleClick)
   }
 
   init() {
     if (this.mobileMenu) {
-      this.addClickEvent();
+      this.addClickEvent()
     }
-    return this;
+    return this
   }
 }
 
 const mobileNavbar = new MobileNavbar(
-  ".mobile-menu",
-  ".nav-list",
-  ".nav-list li"
-);
-mobileNavbar.init();
+  '.mobile-menu',
+  '.nav-list',
+  '.nav-list li'
+)
+mobileNavbar.init()
